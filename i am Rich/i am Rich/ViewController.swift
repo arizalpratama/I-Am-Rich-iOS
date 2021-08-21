b8647082ae2c4ad586c3ca3ad9d97083ff5e3171
arizalpratama <arizalpratama01@gmail.com>
2021-08-21 14:44:07 +0700

    Add files via upload
    commits

---
+++ i am Rich/i am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am Rich
+//
+//  Created by Arizal Pratama Tanu Andara on 21/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
